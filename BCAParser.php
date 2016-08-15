e147f3a6ce5f6f0ba3613e8e44e07f57fcc907b0
kadekjayak <kadekjayak@yahoo.co.id>
2016-08-16 02:25:00 +0800

    Init
    Init

---
+++ BCAParser.php
@@ -0,0 +1,209 @@
+<?php
+/**
+ * Handle login request on m.bca
+ *
+ * no long description
+ *
+ * @author     kadekjayak <kadekjayak@yahoo.co.id>
+ * @copyright  2016 kadekjayak
+ * @license   http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
+ * @version    0.1
+ */
+
+define('BCA_PARSER_DEBUG', false);
+
+class BCAParser {
+	
+	private $username;
+	private $password;
+	
+	protected $curlHandle;
+
+	public $_defaultTargets = [
+		'loginUrl' => 'https://m.klikbca.com/login.jsp',
+		'loginAction' => 'https://m.klikbca.com/authentication.do',
+	];
+	protected $isLoggedIn = false;
+	
+	protected $ipAddress;
+	
+	public $_defaultHeaders = array(
+		'GET /login.jsp HTTP/1.1',
+		'Host: m.klikbca.com',
+		'Connection: keep-alive',
+		'Cache-Control: max-age=0',
+		'Upgrade-Insecure-Requests: 1',
+		'User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36',
+		'Accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+		'Accept-Encoding: gzip, deflate, sdch, br',
+		'Accept-Language: en-US,en;q=0.8,id;q=0.6,fr;q=0.4'
+	);
+	
+	/**
+	* The Constructor
+	* this class will make login request to BCA when initialized
+	*
+	* @param string $username
+	* @param string $password
+	*/
+	public function __construct($username, $password)
+	{
+		if( BCA_PARSER_DEBUG == true ) error_reporting(E_ALL);
+		$this->username = $username;
+		$this->password = $password;
+		$this->curlHandle = curl_init();
+		$this->setupCurl();
+		$this->login($this->username, $this->password);
+	}
+	
+	/**
+	* Get ip address, required on login parameters
+	*
+	* @return String;
+	*/
+	private function getIpAddress()
+	{
+		if($this->ipAddress !== null) $this->ipAddress = json_decode( file_get_contents( 'http://myjsonip.appspot.com/' ) )->ip;
+		return $this->ipAddress;
+
+	}
+	
+	/**
+	* Execute the CURL and return result
+	*
+	* @return curl result
+	*/
+	public function exec()
+	{
+		$result = curl_exec($this->curlHandle);
+		if( BCA_PARSER_DEBUG == true ) {
+			print_r($result);
+		}
+		return $result;
+	}
+	
+	/**
+	* Register default CURL parameters
+	*/
+	protected function setupCurl()
+	{
+		curl_setopt( $this->curlHandle, CURLOPT_URL, $this->_defaultTargets['loginUrl'] );
+		curl_setopt( $this->curlHandle, CURLOPT_POST, 0 );
+		curl_setopt( $this->curlHandle, CURLOPT_HTTPGET, 1 );
+		curl_setopt( $this->curlHandle, CURLOPT_HTTPHEADER, $this->_defaultHeaders);
+		curl_setopt( $this->curlHandle, CURLOPT_SSL_VERIFYPEER, false );
+		curl_setopt( $this->curlHandle, CURLOPT_RETURNTRANSFER, true );
+		curl_setopt( $this->curlHandle, CURLOPT_COOKIEFILE,'cookie' );
+		curl_setopt( $this->curlHandle, CURLOPT_COOKIEJAR, 'cookiejar' );
+	}
+	
+	/**
+	* Set request method on CURL to GET 
+	*/
+	protected function curlSetGet()
+	{
+		curl_setopt( $this->curlHandle, CURLOPT_POST, 0 );
+		curl_setopt( $this->curlHandle, CURLOPT_HTTPGET, 1 );
+	}
+	
+	/**
+	* Set request method on CURL to POST 
+	*/
+	protected function curlSetPost()
+	{
+		curl_setopt( $this->curlHandle, CURLOPT_POST, 1 );
+		curl_setopt( $this->curlHandle, CURLOPT_HTTPGET, 0 );
+	}
+	
+	/**
+	* Login to BCA
+	*/
+	private function login($username, $password)
+	{
+		//Just to Get Cookies
+		curl_setopt( $this->curlHandle, CURLOPT_URL, $this->_defaultTargets['loginUrl'] );
+		$this->curlSetGet();
+		$this->exec();
+		
+		//Sending Login Info
+		$this->getIpAddress();
+		$params = array(
+			"value(user_id)={$username}",
+			"value(pswd)={$password}",
+			'value(Submit)=LOGIN',
+			'value(actions)=login',
+			"value(user_ip)={$this->ipAddress}",
+			"user_ip={$this->ipAddress}",
+			'value(mobile)=true',
+			'mobile=true'
+		);
+		$params = implode( '&', $params );
+		$this->curlSetPost();
+		curl_setopt( $this->curlHandle, CURLOPT_URL, $this->_defaultTargets['loginAction'] );
+		curl_setopt( $this->curlHandle, CURLOPT_REFERER, $this->_defaultTargets['loginUrl'] );
+		curl_setopt( $this->curlHandle, CURLOPT_POSTFIELDS, $params );
+		$this->exec();
+		$this->isLoggedIn = true;
+	}
+	
+	/**
+	* Get mutasi rekening pages
+	*
+	* @param string $from 'Y-m-d'
+	* @param string $to 'Y-m-d'
+	* @return string
+	*/
+	public function getMutasiRekening($from, $to)
+	{
+		if( !$this->isLoggedIn ) $this->login( $this->username, $this->password );
+		
+		$this->curlSetPost();
+		
+		curl_setopt( $this->curlHandle, CURLOPT_URL, 'https://m.klikbca.com/accountstmt.do?value(actions)=menu' );
+		curl_setopt( $this->curlHandle, CURLOPT_REFERER, $this->_defaultTargets['loginAction'] );
+		$this->exec();
+
+		curl_setopt( $this->curlHandle, CURLOPT_URL, 'https://m.klikbca.com/accountstmt.do?value(actions)=acct_stmt' );
+		curl_setopt( $this->curlHandle, CURLOPT_REFERER, 'https://m.klikbca.com/accountstmt.do?value(actions)=menu' );
+		$this->exec();
+		
+		$params = array( 
+				'r1=1', 
+				'value(D1)=0', 
+				'value(startDt)=' . date( 'j', strtotime($from) ), 
+				'value(startMt)=' . date( 'n', strtotime($from) ), 
+				'value(startYr)=' . date( 'Y', strtotime($from) ),
+				'value(endDt)=' . date( 'j', strtotime($to) ),
+				'value(endMt)=' . date( 'n', strtotime($to) ), 
+				'value(endYr)=' . date( 'Y', strtotime($to) ) 
+				);
+		$params = implode( '&', $params );
+		
+		curl_setopt( $this->curlHandle, CURLOPT_URL, 'https://m.klikbca.com/accountstmt.do?value(actions)=acctstmtview' );
+		curl_setopt( $this->curlHandle, CURLOPT_REFERER, 'https://m.klikbca.com/accountstmt.do?value(actions)=acct_stmt' );
+		curl_setopt( $this->curlHandle, CURLOPT_POSTFIELDS, $params );
+		$html = $this->exec();
+		
+		return $this->getMutasiRekeningTable($html);
+	}
+	
+	/**
+	* Parse the pages on mutasi rekening
+	* this method will return only elements on <table> tag that contain only list of transaction
+	*
+	* @param string $html
+	* @return string
+	*/
+	private function getMutasiRekeningTable($html)
+	{
+		$dom = new DOMDocument();
+	
+		$dom->loadHTML($html);
+		$dom->getElementById('pagebody');
+		
+		$table = $dom->getElementsByTagName('table');
+		$table = $table->item(4);
+		return $dom->saveHTML($table);
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,30 @@
+# KlikBCA Parser
+Class ini berfungsi untuk login dan mengambil data daftar mutasi rekening di KlikBCA (m.klikbca.com)
+
+## Installation
+download aja class nya trus include di project php mu.
+	
+	require 'BCAParser.php';
+
+##Requirements
+* PHP curl
+* PHP openssl
+
+
+## Example
+fungsi kelas ini cuma 2 yaitu Login, dan mengambil table data transaksi berdasarkan range tanggal tertentu
+
+### Login
+ketika class ini di di buat, secara otomatis ia akan login ke klik bca melalui CURL
+
+	$Parser = new BCAParser('username', 'password');
+	
+### Mengambil Mutasi Rekening
+mengambil mutasi rekening dapat menggunakan method `getMutasiRekening` dengan parameter range tanggal transaksi yang diinginkan `getMutasiRekening(dari, sampai)`. Contoh :
+	
+	$Html = $Parser->getMutasiRekening('2016-08-1', '2016-08-12');
+
+method ini me return element html <table> yang berisikan daftar transaksi
+
+## Notes
+Untuk debug atau menampilkan response CURL nya, cukup ubah value `BCA_PARSER_DEBUG` pada class ini menjadi `true`
